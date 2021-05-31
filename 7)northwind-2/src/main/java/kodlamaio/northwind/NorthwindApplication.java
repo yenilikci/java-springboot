106c0bc542405261fd53f76fef973f9b2a2bcb52
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-01 00:50:49 +0300

    day 7 - commit2 - dependency simplification
    springfpx-boot-starter'a gerek yokmuş ayrıca basePackage elle verildi

--- 7)northwind-2/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ 7)northwind-2/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,8 +19,10 @@ public class NorthwindApplication {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))
+				.build();
 	}
 
 }
