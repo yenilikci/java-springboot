bc6d7bf469a1e88683545d9395de84a9bf4ad4fc
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-01 00:43:15 +0300

    Day 7 - commit1 - add dependency and annotation
    Maven Dependency, Spring Dependency , Swagger UI pom.xml'e eklendi. Başlangıç java dosyasında importlar ve anatasyonlar yapıldı.

---
+++ 7)northwind-2/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,32 @@
+package kodlamaio.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+//website.com/api/products
+public class ProductsController {
+
+	private ProductService productService;
+
+	@Autowired
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	// website.com/api/products/getall
+	@GetMapping("/getall")
+	public List<Product> getAll() {
+		return this.productService.getAll();
+	}
+
+}
