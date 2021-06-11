5d3db9833b5d8553f99cc61d4c8689d69c2ee8d4
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-12 02:06:34 +0300

    Day 7 updated

--- 7)northwind-2/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ 7)northwind-2/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @RestController
@@ -23,10 +27,16 @@ public class ProductsController {
 		this.productService = productService;
 	}
 
-	// website.com/api/products/getall
+	// http://127.0.0.1:8080//api/products/getall
 	@GetMapping("/getall")
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		return this.productService.getAll();
+	}
+
+	// http://127.0.0.1:8080//api/products/add
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);
 	}
 
 }
--- 7)northwind-2/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ 7)northwind-2/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,10 +2,16 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
 
-	List<Product> getAll();
+	// Product Listesi
+	DataResult<List<Product>> getAll();
+
+	// Product Ekleme
+	Result add(Product product);
 
 }
